d2950df2485369db25e06e563783a67f2de66e21
Mariah <mariahschock@gmail.com>
2022-08-05 12:32:16 -0700

    second GET test is passing for fruits

--- __tests__/fruits.test.js
+++ __tests__/fruits.test.js
@@ -19,6 +19,17 @@ describe('backend-express-template routes', () => {
     }]));
   });
 
+  it('GET - /fruits/:id should return a single fruit', async () => {
+    const res = await request(app).get('/fruits/1');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: '1',
+      name: 'Orange',
+      color: 'orange',
+      goes_on_pizza: false,
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
